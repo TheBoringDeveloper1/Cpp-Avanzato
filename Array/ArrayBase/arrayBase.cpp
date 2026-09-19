abd0fe74bc55723ebf114b4ba35a6a335513f3dd
Devvx <manuelcacciamani1@gmail.com>
2026-09-19 16:25:07 +0200

    Add files via upload

---
+++ Array/ArrayBase/arrayBase.cpp
@@ -0,0 +1,27 @@
+// Includo le direttive
+
+#include <iostream>
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Dichiarazione e inizializzazione array
+
+    int list[] = {-2,3,5,12,7};
+    int i;
+
+    // Ciclo per scorrere elementi array
+
+    for (i = 0; i < 5; i++) {
+
+        // Stampa elementi
+
+        cout << list[i] << endl;
+
+    }
+
+    return 0;
+    
+}
