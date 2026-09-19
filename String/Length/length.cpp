1e6fd8c6271e96e639e30b359904da76b395691d
Devvx <manuelcacciamani1@gmail.com>
2026-09-19 19:41:08 +0200

    Add files via upload

---
+++ String/Length/length.cpp
@@ -0,0 +1,22 @@
+// Includo le direttive
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Funzione principale(main)
+
+int main() {
+
+    // Inizializzazione e stampa array stringa
+
+    string stringa ("Buona giornata!");
+    cout << "Frase array stringa: " << stringa << endl;
+
+    // Numero di caratteri dell'array
+
+    cout << "\nL'array stringa contiene " << stringa.length() << " caratteri";
+    return 0;
+    
+}
